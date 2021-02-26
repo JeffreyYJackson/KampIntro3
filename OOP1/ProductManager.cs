4a1bdcf4e5667079cbaf0aac2583b7683da956eb
jeffreyj <jeffrey.y.jackson@gmail.com>
2021-02-26 19:11:41 +0300

    Lesson

---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace OOP1
+{
+    public class ProductManager
+    {
+        public void Add(Product product)
+        {
+            Console.WriteLine(product.ProductName + " eklendi");
+        }
+
+        public void Update(Product product)
+        {
+            Console.WriteLine(product.ProductName + " güncellendi");
+        }
+
+        
+    }
+}
