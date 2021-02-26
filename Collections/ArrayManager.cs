04656cbdcb68389f8e56a55f992af6eada3a8b79
jeffreyj <jeffrey.y.jackson@gmail.com>
2021-02-26 20:29:39 +0300

    Lesson

---
+++ Collections/ArrayManager.cs
@@ -0,0 +1,11 @@
+namespace Collections
+{
+    public class ArrayManager
+    {
+        public string[] Add(string[] x, string y)
+        {
+            string[] newArray = new string[x.Length + 1];
+            
+        }
+    }
+}
---
+++ Collections/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] names = new string[] { "Robert", "Nesli", "Jeffrey", "Rusty" };
+
+            
+
+
+
+
+            List<string> isimler2 = new List<string>{"Robert", "Nesli", "Jeffrey", "Rusty" };
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            Console.WriteLine(isimler2[2]);
+            Console.WriteLine(isimler2[3]);
+            isimler2.Add("Nesrin");
+            Console.WriteLine(isimler2[4]);
+            Console.WriteLine(isimler2[0]);
+        }
+    }
+}
